49afa6ddffd44baf39ac27429aed23f47c63c861
yattishr <yattish@gmail.com>
2024-11-26 14:29:19 +0200

    Updating middlerware.ts to correctly configure Clerk webhook.

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 const isPublicRoute = createRouteMatcher([
   '/sign-in(.*)', 
   '/sign-up(.*)',
-  '/api/clerk/webhook(.*)'
+  '/api/clerk/webhook'
 ])
 
 export default clerkMiddleware(async (auth, request) => {

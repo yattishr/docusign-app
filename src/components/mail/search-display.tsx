00c509c6249d4abeb3857ecdf3ee9496064c6c0a
yattishr <yattish@gmail.com>
2025-01-06 14:15:00 +0200

    Bug fixing issue with useMutation on search-display UI component.

--- src/components/mail/search-display.tsx
+++ src/components/mail/search-display.tsx
@@ -3,14 +3,14 @@
 import { useAtom } from 'jotai'
 import React, { useEffect } from 'react'
 import { searchValueAtom } from './search-bar'
-import { api } from '@/trpc/server'
+import { api } from "@/trpc/react";
 import { useDebounceValue } from 'usehooks-ts'
 
 const SearchDisplay = () => {
   const [searchValue] = useAtom(searchValueAtom)  
-  
-  // TO DO: Fix useMutation issue below...
-  // const search = api.account.searchEmails.useMutation();
+ 
+
+  const search = api.account.searchEmails.useMutation();
   const [debouncedSearchValuee] = useDebounceValue(searchValue, 500)
 
   useEffect(() => {
